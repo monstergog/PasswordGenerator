f42e1480e0281a7f637c9f88030ec9d43352c04b
monstergog <amn002@ucsd.edu>
2023-04-20 03:48:11 -0700

    added comments to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
-// Assignment code here
+// Constants for all characters to be used for passwords
 const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
 const upperAlphabet = lowerAlphabet.toUpperCase();
 const numericCharacters = "1234567890";
 const specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
+// Generateds a random string of characters depending on user input
 function generatePassword () {
   var tryAgain = true;
   var passLength = -1;
@@ -14,10 +15,11 @@ function generatePassword () {
   var characterSet = "";
   var password = "";
 
-
+  // Prompts user for password length
   while (tryAgain) {
     passLength = prompt("How many chacters should the password have?\n(8 - 128)", "8");
 
+    // Validates user entry is within criteria
     if (passLength < 8 || isNaN(passLength) || passLength > 128) {
       tryAgain = confirm("Error: Invalid length!\n\nWould you like to try again?");
     } else {
@@ -25,13 +27,16 @@ function generatePassword () {
     }
   }
 
+  // Reset flag for while loop
   tryAgain = true;
+  // Asks for user confirmation for what type of characters are to be used in the password
   while (tryAgain) {
     lowercase = confirm("Do you want to include lowercase characters?");
     uppercase = confirm("Do you want to include uppercase characters?");
     numeric = confirm("Do you want to include numeric characters?");
     special = confirm("Do you want to include special characters?");
 
+    // Validates if at least of of the character types are to be used for the password
     if (lowercase === false && uppercase === false && numeric === false && special === false) {
       tryAgain = confirm("Error: Please choose at least one of the character types\n\nWould you like to try again?");
     } else {
@@ -39,11 +44,13 @@ function generatePassword () {
     }
   }
 
+  // Concatenates selected character types into a single string to be used for password generation
   if (lowercase === true) {characterSet += lowerAlphabet;}
   if (uppercase === true) {characterSet += upperAlphabet;}
   if (numeric === true) {characterSet += numericCharacters;}
   if (special === true) {characterSet += specialCharacters;}
   
+  // Generates password by randomly electing elements within the concatenated string to form password
   for (var i = 0; i < passLength; i++) {
     password += characterSet[Math.floor(Math.random() * characterSet.length)];
   }
